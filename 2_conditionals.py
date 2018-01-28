f0bf2926f67cfabb47dcbcbc29c500c80706ac81
Mohit Swami <gottmohit@gmail.com>
2018-01-28 12:59:25 +0530

    First commit for Python Scripts

---
+++ 2_conditionals.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+a, b = 5, 1
+
+if a < b:
+    print('a ({}) is less than b ({})'.format(a, b))
+else:
+    print('a ({}) is not less than b ({})'.format(a, b))
+
+# print(a < b ? 'foo' : 'bar')
+
+print('foo' if a < b else 'bar')
---
+++ 4_forloop.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+fh = open('lines.txt')
+for line in fh.readlines():
+    print(line, end="")
